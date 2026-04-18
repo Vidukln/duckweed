29e4c7a71ba95213d843cbc1927c14cb67034673
Vidukln <59608320+Vidukln@users.noreply.github.com>
2026-04-18 13:52:50 +0300

    Update model.py

--- model.py
+++ model.py
@@ -2,8 +2,7 @@ import numpy as np
 import cv2
 import pickle
 
-# Load trained model
-with open("model.pkl", "rb") as f:
+with open("gmm_model.pkl", "rb") as f:
     model = pickle.load(f)
 
 
@@ -14,22 +13,18 @@ def analyze_image(image):
     h, w = img.shape[:2]
     pixels = img.reshape(-1, 3)
 
-    # Predict clusters
     labels = model.predict(pixels)
     label_map = labels.reshape(h, w)
 
-    # Identify green cluster (based on training means)
     means = model.means_
-    green_idx = np.argmax(means[:, 1])  # green dominance
 
-    # Mask
-    green_mask = (label_map == green_idx)
+    green_idx = np.argmax(means[:, 1])
 
-    # Coverage
-    coverage = np.sum(green_mask) / green_mask.size
+    mask = (label_map == green_idx)
 
-    # Output image
+    coverage = np.sum(mask) / mask.size
+
     result = img.copy()
-    result[green_mask] = [0, 255, 0]
+    result[mask] = [0, 255, 0]
 
     return result, float(coverage)
